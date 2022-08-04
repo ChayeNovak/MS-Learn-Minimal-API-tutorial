ea778b171251d82f52894f619f2d79ed42dc718d
ChayeNovak <ChayeNovak@users.noreply.github.com>
2022-08-04 10:44:07 +0100

    implementing EF Core
    Added async calls for a list of pizzas

---
+++ Pizza.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+namespace PizzaStore.Models {
+    public class Pizza {
+        public int id {get; set;}
+        public string? Name {get; set;}
+        public string? Description {get; set;}
+    }
+
+    class PizzaDb : DbContext {
+    public PizzaDb(DbContextOptions options) : base(options) {}
+    public DbSet<Pizza> Pizzas {get; set;} = null!;
+}
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using PizzaStore.Models;
 using PizzaStore.DB;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo {Title = "PizzaStore API", Description = "Making the Pizzas you like", Version = "v1"});
@@ -19,11 +21,12 @@ app.UseSwaggerUI(C =>
 });
 
 app.MapGet("/", () => "Hello World!");
+/*Original routes, without EF Core
 app.MapGet("/pizzas/{id}", (int id) => PizzaDB.GetPizza(id));
 app.MapGet("/pizzas", () => PizzaDB.GetPizzas());
-app.MapPost("/pizzas", (Pizza pizza) => PizzaDB.CreatePizza(pizza));
-app.MapPut("/pizzas", (Pizza pizza) => PizzaDB.UpdatePizza(pizza));
-app.MapPut("/pizzas/{id}", (int id) => PizzaDB.RemovePizza(id));
-
+app.MapPost("/pizzas", (PizzaStore.DB.Pizza pizza) => PizzaDB.CreatePizza(pizza));
+app.MapPut("/pizzas", (PizzaStore.DB.Pizza pizza) => PizzaDB.UpdatePizza(pizza));
+app.MapPut("/pizzas/{id}", (int id) => PizzaDB.RemovePizza(id));*/
+app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
 
 app.Run();
