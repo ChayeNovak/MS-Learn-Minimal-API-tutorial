2c1d8a1007f3dc9422d6a4b5f8ae821e2d003d03
ChayeNovak <ChayeNovak@users.noreply.github.com>
2022-08-04 09:51:22 +0100

    Updated db.cs
    Implemented delete functionality

--- Db.cs
+++ Db.cs
@@ -12,26 +12,27 @@ public class PizzaDB {
         new Pizza{Id = 3, Name="The Noring, Pizza shaped like a Viking helmet, where's the mead"}
     };
 
-public static List<Pizza> GetPizzas() {
-    return _pizzas;
-}
+    public static List<Pizza> GetPizzas() {
+        return _pizzas;
+    }
 
-public static Pizza CreatePizza(Pizza pizza) {
-    _pizzas.Add(pizza);
-    return pizza;
-}
-
-public static Pizza UpdatePizza(Pizza update) {
-    _pizzas = _pizzas.Select(pizza =>
-    {
-        if (pizza.Id == update.Id) {
-            pizza.Name = update.Name;
-        }
+    public static Pizza CreatePizza(Pizza pizza) {
+        _pizzas.Add(pizza);
         return pizza;
-    }).ToList();
-    return update;
-}
+    }
 
+    public static Pizza UpdatePizza(Pizza update) {
+        _pizzas = _pizzas.Select(pizza =>
+        {
+            if (pizza.Id == update.Id) {
+                pizza.Name = update.Name;
+            }
+            return pizza;
+        }).ToList();
+        return update;
+    }
 
-
+    public static void RemovePizza(int id) {
+        _pizzas = _pizzas.FindAll(pizza => pizza.Id != id).ToList();
+    }
 }
